6a14fc3d6ac2cc52d7037e695f40cef602091851
Richie Bendall <richiebendall@gmail.com>
2020-10-25 16:47:02 +1300

    Add `.multiple()` method
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- index.js
+++ index.js
@@ -7,3 +7,21 @@ module.exports = array => {
 
 	return array[Math.floor(Math.random() * array.length)];
 };
+
+module.exports.multiple = (array, count) => {
+	if (!Array.isArray(array)) {
+		throw new TypeError('Expected an array');
+	}
+
+	if (!Number.isInteger(count) && count >= 0) {
+		throw new TypeError('Expected a non-negative integer');
+	}
+
+	if (count > array.length) {
+		throw new TypeError(`Expected an array with at least ${count} items`);
+	}
+
+	array = [...array];
+
+	return [...new Array(count)].map(() => array.splice(Math.floor(Math.random() * array.length), 1)[0]);
+};
--- index.test-d.ts
+++ index.test-d.ts
@@ -3,3 +3,6 @@ import randomItem = require('.');
 
 expectType<string>(randomItem(['🐴', '🦄', '🌈']));
 expectType<string | number>(randomItem(['🐴', '🦄', '🌈', 1]));
+
+expectType<string[]>(randomItem.multiple(['🐴', '🦄', '🌈'], 2));
+expectType<Array<string | number>>(randomItem.multiple(['🐴', '🦄', '🌈', 1], 2));
